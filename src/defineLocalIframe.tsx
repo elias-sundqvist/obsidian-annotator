6c64cf2d4181a7526ed4f8b83ce34e5b86ccc48a
Elias Sundqvist <elias@sundqvist.com>
2021-09-12 00:41:13 +0200

    Add basic CORS workaround
    #15

--- src/defineLocalIframe.tsx
+++ src/defineLocalIframe.tsx
@@ -9,6 +9,60 @@ import { DarkReaderType } from 'darkreader';
 import { LocalIFrameProps } from 'types';
 import { WebSocket, Server } from 'mock-websocket';
 
+async function httpsGet(url): Promise<Buffer> {
+    console.log(`Getting ${url} using httpsGet`);
+    function get(url, resolve, reject) {
+        global.require('https').get(
+            url,
+            {
+                headers: {
+                    Accept: '*/*',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'User-Agent':
+                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) obsidian/0.12.3 Chrome/89.0.4389.128 Electron/12.0.6 Safari/537.36'
+                }
+            },
+            res => {
+                // if any other status codes are returned, those needed to be added here
+                if (res.statusCode === 301 || res.statusCode === 302) {
+                    return get(res.headers.location, resolve, reject);
+                }
+
+                const data = [];
+
+                res.on('data', function (chunk) {
+                    data.push(chunk);
+                }).on('end', function () {
+                    resolve(Buffer.concat(data));
+                });
+            }
+        );
+    }
+    return await new Promise((resolve, reject) => get(url, resolve, reject));
+}
+
+const corsFetch = async (...args: Parameters<typeof fetch>) => {
+    try {
+        const res = await fetch(...args);
+        if (!res.ok) {
+            throw 'Fetch was not successful.';
+        }
+        return res;
+    } catch (e) {
+        try {
+            const buf = await httpsGet(args[0]);
+            return new Response(buf, {
+                status: 200,
+                statusText: 'ok'
+            });
+        } catch (e2) {
+            const combinedError = new AggregateError([e, e2], `Fetching url ${args[0]} failed!`);
+            console.error(combinedError);
+            throw combinedError;
+        }
+    }
+};
+
 const hypothesisFolder = jszip.loadAsync(hypothesisResources);
 
 export default ({ vault, resourceUrls }: { vault: Vault; resourceUrls: Map<string, string> }) => {
@@ -272,7 +326,7 @@ export default ({ vault, resourceUrls }: { vault: Vault; resourceUrls: Map<strin
                     return new Response(null, { status: 404, statusText: 'file not found' });
                 }
             }
-            return fetch(url.toString());
+            return corsFetch(url.toString());
         }
 
         function proxySrc(src) {
