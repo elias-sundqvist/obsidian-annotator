88d8ef6413f7718ccd602ca2c5926fae5aec2f39
Andrey Aleksandrov <aleksandrov@hey.com>
2022-02-27 21:27:28 +0300

    Fix rendition.on event + code format

--- src/defineEpubAnnotation.tsx
+++ src/defineEpubAnnotation.tsx
@@ -37,85 +37,84 @@ export default ({ vault, plugin, resourceUrls }) => {
                         // var hash = window.location.hash.slice(2);
                         var loc = iframe.contentWindow.location.href.indexOf("?loc=");
                         if (loc > -1) {
-                          var href =  iframe.contentWindow.location.href.slice(loc + 5);
-                          var hash = decodeURIComponent(href);
+                            var href =  iframe.contentWindow.location.href.slice(loc + 5);
+                            var hash = decodeURIComponent(href);
                         }
 
                         rendition.display(hash || undefined);
 
                         switch (plugin.settings.epubSettings.readingMode) {
-                          case "scroll":
-                            iframe.contentDocument.querySelectorAll("a.arrow").forEach((e) => e.remove());
-                            iframe.contentDocument.querySelector("#viewer").classList.add("hide-after");
-                            break;
+                            case "scroll":
+                                iframe.contentDocument.querySelectorAll("a.arrow").forEach((e) => e.remove());
+                                iframe.contentDocument.querySelector("#viewer").classList.add("hide-after");
+                                break;
 
-                          case "pagination":
-                            var next = iframe.contentDocument.getElementById("next");
-                            next.addEventListener("click", function(e){
-                              rendition.next();
-                              e.preventDefault();
-                            }, false);
+                            case "pagination":
+                                var next = iframe.contentDocument.getElementById("next");
+                                next.addEventListener("click", function(e){
+                                    rendition.next();
+                                    e.preventDefault();
+                                }, false);
 
-                            var prev = iframe.contentDocument.getElementById("prev");
-                            prev.addEventListener("click", function(e){
-                              rendition.prev();
-                              e.preventDefault();
-                            }, false);
-
-                            break;
+                                var prev = iframe.contentDocument.getElementById("prev");
+                                prev.addEventListener("click", function(e){
+                                    rendition.prev();
+                                    e.preventDefault();
+                                }, false);
+                                break;
                         }
 
                         var nav = iframe.contentDocument.getElementById("navigation");
                         var opener = iframe.contentDocument.getElementById("opener");
                         opener.addEventListener("click", function(e){
-                          nav.classList.add("open");
+                            nav.classList.add("open");
                         }, false);
 
                         var closer = iframe.contentDocument.getElementById("closer");
                         closer.addEventListener("click", function(e){
-                          nav.classList.remove("open");
+                            nav.classList.remove("open");
                         }, false);
 
                         // Hidden
                         var hiddenTitle = iframe.contentDocument.getElementById("hiddenTitle");
 
                         rendition.on("rendered", function(section){
-                          var current = book.navigation && book.navigation.get(section.href);
+                            var current = book.navigation && book.navigation.get(section.href);
 
-                          if (current) {
-                            iframe.contentDocument.title = current.label;
-                          }
+                            if (current) {
+                                iframe.contentDocument.title = current.label;
 
-                          // TODO: this is needed to trigger the hypothesis client
-                          // to inject into the iframe
-                          requestAnimationFrame(function () {
-                            hiddenTitle.textContent = section.href;
-                          })
+                                // TODO: this is needed to trigger the hypothesis client
+                                // to inject into the iframe
+                                requestAnimationFrame(function () {
+                                  hiddenTitle.textContent = section.href;
+                                })
 
-                          var old = iframe.contentDocument.querySelectorAll('.active');
-                          Array.prototype.slice.call(old, 0).forEach(function (link) {
-                            link.classList.remove("active");
-                          })
+                                // Add CFI fragment to the history
+                                history.pushState({}, '', "?loc=" + encodeURIComponent(section.href));
+                                // window.location.hash = "#/"+section.href
 
-                          var active = iframe.contentDocument.querySelector('a[href="'+section.href+'"]');
-                          if (active) {
-                            active.classList.add("active");
-                          }
-                          // Add CFI fragment to the history
-                          history.pushState({}, '', "?loc=" + encodeURIComponent(section.href));
-                          // window.location.hash = "#/"+section.href
+                                var old = iframe.contentDocument.querySelectorAll('.active');
+                                Array.prototype.slice.call(old, 0).forEach(function (link) {
+                                    link.classList.remove("active");
+                                })
+
+                                var active = iframe.contentDocument.querySelector('a[href="'+section.href+'"]');
+                                if (active) {
+                                    active.classList.add("active");
+                                }
+                            }
                         });
 
                         var keyListener = function(e){
-
                           // Left Key
                           if ((e.keyCode || e.which) == 37) {
-                            rendition.prev();
+                              rendition.prev();
                           }
 
                           // Right Key
                           if ((e.keyCode || e.which) == 39) {
-                            rendition.next();
+                              rendition.next();
                           }
                         };
 
@@ -123,8 +122,8 @@ export default ({ vault, plugin, resourceUrls }) => {
                         iframe.contentDocument.addEventListener("keyup", keyListener, false);
 
                         book.ready.then(function () {
-                          var $viewer = iframe.contentDocument.getElementById("viewer");
-                          $viewer.classList.remove("loading");
+                            var $viewer = iframe.contentDocument.getElementById("viewer");
+                            $viewer.classList.remove("loading");
                         });
 
                         book.loaded.navigation.then((nav:any) => {
@@ -152,13 +151,13 @@ export default ({ vault, plugin, resourceUrls }) => {
                         });
 
                         book.loaded.metadata.then(function(meta){
-                          var $title = iframe.contentDocument.getElementById("title");
-                          var $author = iframe.contentDocument.getElementById("author");
-                          var $cover = iframe.contentDocument.getElementById("cover");
-                          var $nav = iframe.contentDocument.getElementById('navigation');
+                            var $title = iframe.contentDocument.getElementById("title");
+                            var $author = iframe.contentDocument.getElementById("author");
+                            var $cover = iframe.contentDocument.getElementById("cover");
+                            var $nav = iframe.contentDocument.getElementById('navigation');
 
-                          $title.textContent = meta.title;
-                          $author.textContent = meta.creator;
+                            $title.textContent = meta.title;
+                            $author.textContent = meta.creator;
 
                             book.loaded.cover.then((cover) => {
                                 if (cover) {
@@ -174,15 +173,15 @@ export default ({ vault, plugin, resourceUrls }) => {
                         });
 
                         book.rendition.hooks.content.register(function(contents, view) {
-                          contents.window.addEventListener('scrolltorange', function (e) {
-                            var range = e.detail;
-                            var cfi = new epubjs.EpubCFI(range, contents.cfiBase).toString();
+                            contents.window.addEventListener('scrolltorange', function (e) {
+                                var range = e.detail;
+                                var cfi = new epubjs.EpubCFI(range, contents.cfiBase).toString();
 
-                            if (cfi) {
-                              book.rendition.display(cfi);
-                            }
-                            e.preventDefault();
-                          });
+                                if (cfi) {
+                                  book.rendition.display(cfi);
+                                }
+                                e.preventDefault();
+                            });
                         });
                     }
 
