9c8810ed9f74f5d6eb4a61520ad8ea712991e94b
Elias Sundqvist <elias@sundqvist.com>
2022-08-07 16:19:01 +0200

    Reintroduce node-fetch

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,15 +1,57 @@
 import { requestUrl } from 'obsidian';
 
+import defineNodeFetch from './node-fetch';
+
+const nodeFetch = (() => {
+    type FetchType = typeof fetch;
+    const pureNodeFetch = (() => {
+        try {
+            return defineNodeFetch(x => {
+                try {
+                    //console.log(`getting module "${x}"`);
+                    if (x.startsWith('node:')) {
+                        return global.require(x.substr('node:'.length));
+                    } else {
+                        return global.require(x);
+                    }
+                } catch (e) {
+                    //console.log(`failed to get module "${x}"`);
+                }
+            });
+        } catch (e) {
+            //console.log(e);
+            return null;
+        }
+    })() as null | FetchType;
+    if (pureNodeFetch) {
+        return async (requestInfo: RequestInfo, requestInit?: RequestInit) => {
+            const response: Response = await pureNodeFetch(requestInfo, requestInit);
+            if (response.ok) {
+                return new Response(await response.arrayBuffer(), {
+                    ...response
+                });
+            }
+        };
+    }
+})();
+
 // This fetch can be used to get internal(like blob) and external resources with CORS policies
 export async function fetchUrl(requestInfo: RequestInfo, requestInit?: RequestInit): Promise<Response> {
-
+    console.log("fetching", requestInfo, requestInit);
     // Use regular fetch for blobs, because obsidian.requestUrl can't access files by path
     if (requestInfo.toString().startsWith('blob:')) return await fetch(requestInfo, requestInit);
 
     try {
+        const requestHeaders = new Headers(requestInit?.headers);
         const response = await requestUrl({
             url: requestInfo instanceof Request ? requestInfo.url : requestInfo,
-            method: requestInit?.method
+            ...(requestInit?Object.fromEntries(Object.entries({
+                method: requestInit.method,
+                contentType: requestHeaders.get('Content-Type'),
+                body: requestInit.body,
+                headers: Object.fromEntries(requestHeaders?.entries()),
+                throw: true
+            }).filter(kv=>kv[1]!==null)):{})
         });
 
         return new Response(response.arrayBuffer, {
@@ -18,8 +60,12 @@ export async function fetchUrl(requestInfo: RequestInfo, requestInit?: RequestIn
             headers: new Headers(response.headers)
         });
     } catch (e) {
-        // fallback to regular fetch, because requestUrl sometimes fails on iPad
-        return await fetch(requestInfo, requestInit);
+        try {
+            return await nodeFetch(typeof requestInfo === 'string' ? requestInfo : requestInfo.url, requestInit);
+        } catch(e2) {
+            // fallback to regular fetch, because requestUrl sometimes fails on iPad
+            return await fetch(requestInfo, requestInit);
+        }
     }
 }
 
